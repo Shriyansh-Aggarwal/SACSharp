21b735010ebd49343d6b72548b05d3eadc9d3511
Shriyansh-Aggarwal <shriyansh.agg@gmail.com>
2025-04-15 17:02:06 +0530

    Added Tests

---
+++ Tests/HardcodedPasswordTest.cs
@@ -0,0 +1,10 @@
+﻿public class HardcodedPasswordTest
+{
+    public void Login()
+    {
+        string password = "1234567890";
+        string admin = "pass";
+        string pwd = "test";
+        string x = "pwdtest";
+    }
+}
